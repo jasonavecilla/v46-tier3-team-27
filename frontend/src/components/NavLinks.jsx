898fba0ac2892989b148612e79e16a3492105ad5
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-10-10 16:10:43 +0900

    Restrict access to menu

--- frontend/src/components/NavLinks.jsx
+++ frontend/src/components/NavLinks.jsx
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { NavLink } from "react-router-dom";
 const links = [
@@ -9,10 +10,12 @@ const links = [
   // { id: 6, url: 'orders', text: 'orders' },
 ];
 const NavLinks = () => {
+  const { user } = useAuth0();
   return (
     <>
       {links.map((link) => {
         const { id, url, text } = link;
+        if (!user && url === "menu") return;
         return (
           <li key={id}>
             <NavLink className="capitalize" to={url}>
